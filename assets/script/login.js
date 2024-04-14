f26036a662a4ec8ddccf05b7f3c75fb9e007b4c6
rizqi ahong <risqinur28@gmail.com>
2024-04-15 02:20:39 +0700

    update 2 commit

---
+++ assets/script/login.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const passwordInput = document.getElementById("password");
+  const btnPassword = document.getElementById("toogle-password");
+
+  const eye = document.getElementById("eye");
+
+  btnPassword.addEventListener("click", () => {
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      eye.classList.remove("fa", "fa-eye-slash");
+      eye.classList.add("fa", "fa-eye");
+    } else {
+      passwordInput.type = "password";
+      eye.classList.remove("fa", "fa-eye");
+      eye.classList.add("fa", "fa-eye-slash");
+    }
+  });
+});
