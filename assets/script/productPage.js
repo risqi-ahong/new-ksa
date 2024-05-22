64f62b45ef4c9354d8ffbb40d0fc42fc788b77b4
risqi-ahong <65552358+risqi-ahong@users.noreply.github.com>
2024-05-22 23:22:09 +0700

    tambah dikit aja 3

--- assets/script/productPage.js
+++ assets/script/productPage.js
@@ -1,8 +1,55 @@
 const body = document.querySelector("body");
 
-// const item = document.querySelectorAll(".product-page-item");
-
 const totalContent = document.querySelector(".total-content-img");
+
+const listSpesifikasi = document.querySelectorAll("li");
+const linkSpesifikasi = document.querySelector(".link-spesifikasi");
+const listParagraf = document.createElement("p");
+
+const spesifikasi = document.querySelector(".spesifikasi-product");
+
+const read = document.querySelector(".read");
+const deskripsi = document.querySelector(".deskripsi");
+const btnRead = document.querySelector(".btn-read");
+const btnIcon = document.querySelector("#btn-icon");
+
+if (deskripsi.clientHeight < 208) {
+  btnRead.style.display = "none";
+  read.style.border = "none";
+} else {
+  console.log(btnIcon.classList.add("fa fa-chevron-down"));
+  console.log(btnIcon);
+  btnRead.style.display = "inline-block";
+  deskripsi.style.height = "12rem";
+  document.addEventListener("click", (e) => {
+    if (btnRead.contains(e.target)) {
+      if (deskripsi.clientHeight <= 208) {
+        deskripsi.style.height = "auto";
+        btnRead.innerHTML = "Hide";
+      } else {
+        deskripsi.style.height = "12rem";
+        btnRead.innerHTML = "In Full";
+      }
+    }
+  });
+}
+const listArr = [...listSpesifikasi];
+let list = listArr.map((listArr) => listArr.textContent);
+list = list.slice(1);
+
+listParagraf.innerHTML = list;
+
+linkSpesifikasi.appendChild(listParagraf);
+
+listParagraf.onclick = () => {
+  spesifikasi.classList.toggle("active");
+};
+
+document.addEventListener("click", (e) => {
+  if (!spesifikasi.contains(e.target) && !listParagraf.contains(e.target)) {
+    spesifikasi.classList.remove("active");
+  }
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   const prevBtn = document.querySelector(".prev");
